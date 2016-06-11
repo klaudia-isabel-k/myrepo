43f3a59624ffd0ac89b3fad2755302725e966346
Klaudia <klaudia.isabel.k@gmail.com>
2016-06-11 15:03:06 +0100

    Plot to the Exercise

---
+++ exercise.py
@@ -0,0 +1,53 @@
+import openpyxl
+import pandas as pd
+import numpy as np
+import math
+import datetime as dt
+import pylab as pl
+
+
+# FUNCTION #
+
+def log_returns(price_t, price_1_t):
+    try:
+        return np.log(price_t/price_1_t)
+    except:
+        return None
+
+# RUN #
+
+def main():
+    df = pd.read_excel('PythonTrainingStockPrices_Input.xlsx','Table')
+
+    df['Nike_t_1'] = df['Nike'].shift(-1)
+    df['Apple_t_1'] = df['Apple'].shift(-1)
+
+    df['Nike_return'] = df.apply(lambda x: log_returns(x.Nike, x.Nike_t_1), axis=1)
+    df['Apple_return'] = df.apply(lambda x: log_returns(x.Apple, x.Apple_t_1), axis=1)
+
+    df.to_excel('PythonTrainingStockPrices_Output.xlsx','Table',header=True)
+
+    nike_count = df['Nike'].count()
+    apple_count = df['Apple'].count()
+
+    nike_sum = df['Nike_return'].sum()
+    apple_sum = df['Apple_return'].sum()
+
+    df['Date'] = df['Date'].dt.strftime('%Y-%M-%D')
+
+    # plot the data
+    ax = df[-50:-1].plot(figsize=(10, 6),
+                               title = 'Returns : Apple and Nike Returns over year 2015.',
+                               x = ['Date'],
+                               y = ['Nike_return', 'Apple_return'],
+                               color = ['blue', 'cyan'],
+                               kind = 'bar')
+
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Stock Return")
+
+    pl.show()
+
+main()
+
+
