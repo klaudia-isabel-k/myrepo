65a03685d221b1559b9db965fb50780b5f904149
Klaudia <klaudia.isabel.k@gmail.com>
2016-06-11 15:41:32 +0100

    VAR, STD, COV

--- exercise.py
+++ exercise.py
@@ -27,11 +27,34 @@ def main():
 
     df.to_excel('PythonTrainingStockPrices_Output.xlsx','Table',header=True)
 
+    #Calculate Count of Stock Data Points
+
     nike_count = df['Nike'].count()
     apple_count = df['Apple'].count()
 
+    #Calculate Sum of Stock Returns
+
     nike_sum = df['Nike_return'].sum()
     apple_sum = df['Apple_return'].sum()
+
+    #Calculate Variance of Stock Returns
+
+    nike_var = df['Nike_return'][:-1].var()
+    apple_var = df['Apple_return'][:-1].var()
+
+    #Calculate Standard Deviation of Stock Returns
+
+    nike_std = df['Nike_return'][:-1].std()
+    apple_std = df['Apple_return'][:-1].std()
+
+    #Calculate Covariance of Stocks
+
+    nike_cov = df[['Nike']].cov()
+    apple_cov = df[['Apple']].cov()
+
+    #Calculate 99% of Historical Value at Risk
+
+    
 
     df['Date'] = df['Date'].dt.strftime('%Y-%M-%D')
 
